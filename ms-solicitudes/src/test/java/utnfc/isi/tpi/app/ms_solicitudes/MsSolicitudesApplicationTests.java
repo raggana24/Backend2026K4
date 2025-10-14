6fdc263c106bb124b938e51325c18292578bed2b
raggana24 <gabrieldagna123@gmail.com>
2025-10-14 11:15:09 -0300

    Trabajo del TP

---
+++ ms-solicitudes/src/test/java/utnfc/isi/tpi/app/ms_solicitudes/MsSolicitudesApplicationTests.java
@@ -0,0 +1,13 @@
+package utnfc.isi.tpi.app.ms_solicitudes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSolicitudesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
